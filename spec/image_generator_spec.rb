bdb38ac10d77d3dc15c5646c65c8fb02179fc183
Matt Kragen <mattdkragen@gmail.com>
2021-05-25 10:14:59 -0700

    Add tests for generate_images & change_max_size

--- spec/image_generator_spec.rb
+++ spec/image_generator_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe ImageGenerator do
       expect(image_generator.max_image_size).to eq 2500
     end
 
+    it 'generates images' do
+      image_generator = ImageGenerator.new(2500)
+      allow(image_generator).to receive(:rand).and_return(1)
+
+      expect(image_generator.generate_images).to eq(["image0"])
+    end
+
+    it 'changes max size' do
+      image_generator = ImageGenerator.new(2500)
+      allow(image_generator).to receive(:rand).and_return(1)
+
+      expect(image_generator.change_max_size(5)).to eq(5)
+    end
     # Using stubs write the tests to confirm the functionality
     # that exists in ./lib/image_generator.rb
   end
